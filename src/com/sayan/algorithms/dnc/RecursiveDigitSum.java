15d3e40ecf547d6099c5c6d248a976d04c562421
Sayan Guha <sayanguha99@gmail.com>
2020-04-11 10:26:44 +0530

    Organize imports for RecursiveDigitSum.java

--- src/com/sayan/algorithms/dnc/RecursiveDigitSum.java
+++ src/com/sayan/algorithms/dnc/RecursiveDigitSum.java
@@ -1,12 +1,7 @@
 package com.sayan.algorithms.dnc;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class RecursiveDigitSum {
 	static int superDigit(String n, int k) {
